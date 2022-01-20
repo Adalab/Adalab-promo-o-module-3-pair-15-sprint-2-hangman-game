8a6bdb9b6d93b8725b5b6e631f9f95bc6bba80f9
Okia02 <anajimenezmelero@gmail.com>
2022-01-20 11:26:25 +0100

    comment on functionalities

--- src/components/App.js
+++ src/components/App.js
@@ -30,38 +30,71 @@ function App() {
   const handleButtonClick = () => {
     setNumberOfErrors(numberOfErrors + 1);
   };
+
+  //Recoger el input del usuario
   const handleInputChange = (ev) => {
+    //Evita el envío del formulario
     ev.preventDefault();
+    //Recoge el valor del input
     const inputValue = ev.currentTarget.value;
+    //Expresión regular para solo aceptar caracteres permitidos
     const regex = new RegExp("^[a-zA-Z\u00C0-\u00FF]?$");
+    //Si es un caracter permitido y no es algo en blanco o un espacio,
     if (regex.test(inputValue) && inputValue !== "") {
+      //el input se guarda en lastLetter
       setLastLetter(inputValue);
+      //Si la palabra correcta incluye lastLetter,
       if (word.includes(inputValue)) {
+        //se guarda en el array de letras correctas (userLetters)
         setUserLetters([...userLetters, inputValue]);
       } else {
+        //Si no es una letra correcta, se guarda en el array de letras incorrectas (incorrectLetters)
         setIncorrectLetters([...incorrectLetters, inputValue]);
       }
     } else {
+      //Después, se limpia lastLetter
       setLastLetter("");
     }
   };
 
+  //Pintar letras correctas
   const renderSolutionLetters = () => {
+    //convertir la palabra (word, que es un string), en un array con elementos separados (cada letra)
     const wordLetters = word.split("");
     // Pintar el guión de cada letra
     return wordLetters.map((eachLetter, index) => {
+      //Si la letra es correcta, 
       if (userLetters.includes(eachLetter)) {
         return (
+          //pinta la letra en solución
           <li className="letter" key={index}>
             {eachLetter}
           </li>
         );
       } else {
+        //si no lo es, no pinta nada
         return <li className="letter" key={index}></li>;
       }
     });
   };
-  //const renderErrorLetters = () => {};
+
+  const renderErrorLetters = () => {
+    //El map recorre el array de letras incorrectas
+    return incorrectLetters.map((eachLetter, index) => {
+      //Si la letra incorrecta está incluida,
+        if (incorrectLetters.includes(eachLetter)) {
+          return (
+            //pinta la letra y la rayita
+            <li className="letter" key={index}>
+              {eachLetter}
+            </li>
+          );
+        } else {
+          //si no, no pinta nada
+          return <li className="letter" key={index}></li>;
+        }
+      });
+  };
 
   return (
     <div>
@@ -75,26 +108,12 @@ function App() {
               <h2 className="title">Solución:</h2>
               <ul className="letters">
                 {renderSolutionLetters()}
-                {/* <li className="letter">k</li>
-                <li className="letter">a</li>
-                <li className="letter"></li>
-                <li className="letter">a</li>
-                <li className="letter">k</li>
-                <li className="letter">r</li>
-                <li className="letter"></li>
-                <li className="letter">k</li>
-                <li className="letter">e</li>
-                <li className="letter">r</li> */}
               </ul>
             </div>
             <div className="error">
               <h2 className="title">Letras falladas:</h2>
               <ul className="letters">
-                <li className="letter">f</li>
-                <li className="letter">q</li>
-                <li className="letter">h</li>
-                <li className="letter">p</li>
-                <li className="letter">x</li>
+                {renderErrorLetters()}
               </ul>
             </div>
             <form className="form">
@@ -127,7 +146,7 @@ function App() {
             <span className="error-3 line"></span>
             <span className="error-2 line"></span>
             <span className="error-1 line"></span>
-            <button onClick={handleButtonClick}>Incrementar</button>
+            {/*<button onClick={handleButtonClick}>Incrementar</button>*/}
           </section>
         </main>
       </div>
