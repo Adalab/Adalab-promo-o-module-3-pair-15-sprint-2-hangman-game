b7af195c9bc9f7b0b335ca9facab2bbc90c9675a
Okia02 <anajimenezmelero@gmail.com>
2022-01-24 10:37:47 +0100

    add API and component header

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,19 @@
 import "../styles/App.scss";
-import { useState } from "react";
-
-// Al arrancar la página:
-
-// - traer solución de la API (string)
-// - pintar rayitas longitud palabra
-
-// Cuando la usuaria mete 1 letra:
-
-// Verificar si ya existe en array solución/falladas
-
-// Verificar si está dentro de la palabra solución
-
-// - Si lo está >>> que aparezca en la solución tantas veces como se repita
-
-// - Si no lo está >>> que aparezca en los errores (letra y rayita) Y pinte parte del cuerpo del ahorcado Y actualice contador de errores
-
-// RESET: durante todo el juego disponible
-// Si se ha pintado todo el ahorcado (num máx de errores), pierdes, reset, nueva palabra api
-// Si se ha completado la solución, ganas, reset, nueva palabra api
+import { useState, useEffect } from "react";
+import callToApi from '../services/fetch';
+import Header from './Header';
 
 function App() {
-  const [word, setWord] = useState("pepino");
+  const [word, setWord] = useState('');
   const [lastLetter, setLastLetter] = useState("");
   const [userLetters, setUserLetters] = useState([]);
   const [incorrectLetters, setIncorrectLetters] = useState([]);
+
+  useEffect(() => {
+    callToApi().then((response) => {
+      setWord(response);
+    });
+  }, []);
 
   //Recoger el input del usuario
   const handleInputChange = (ev) => {
@@ -100,9 +89,7 @@ function App() {
   return (
     <div>
       <div className="page">
-        <header>
-          <h1 className="header__title">Juego del ahorcado</h1>
-        </header>
+        <Header/>
         <main className="main">
           <section>
             <div className="solution">
